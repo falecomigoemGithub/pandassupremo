098aea882c207b5d5dddc5b3dc8763d1c195d9dd
Angelo AKA frost <falecomigoem@gmail.com>
2026-03-06 10:00:32 -0300

    dia 06/03/2026

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import pandas as pd
+import numpy as np
+from IPython.display import display
+
 def main():
-    print("Hello from pandashashtag!")
-    print("Hello from pandashashtag com git!")
+    df_vendas = pd.read_csv("vendas_tech.csv")
+    display(df_vendas)
 
 
 if __name__ == "__main__":
