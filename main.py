81824cf0d2aa1b066db24a20ee5d4f17d3bde267
Angelo AKA frost <falecomigoem@gmail.com>
2026-03-06 15:37:40 -0300

    Criação do arquivo main.ipynb

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import pandas as pd
-import numpy as np
-from IPython.display import display
 
 def main():
-    df_vendas = pd.read_csv("vendas_tech.csv")
-    display(df_vendas)
-
-
+    pass
 if __name__ == "__main__":
     main()
